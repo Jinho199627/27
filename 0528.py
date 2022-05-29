ad1d8d5c420ff3a779b49292cedf806f799cfdc6
Jinho_Seo <j0418ok@naver.com>
2022-05-29 14:26:15 +0900

    12 commit

--- 0528.py
+++ 0528.py
@@ -57,19 +57,23 @@ import sys
 
 
 
-# 1157번
+# 1157번(답은 맞게 나오는데 시간 초과됐다. 다시 풀어보자.)
+# 22년 5월 29일에 풀이 완료.
 input_value = sys.stdin.readline().strip().upper()
-input_list = []
+input_dict = dict()
 answer_list = []
 for i in str(input_value):
-    input_list.append(i)
-for i in input_list:
-    answer_list.append(input_list.count(i))
-answer_set = set()
-for i in range(len(answer_list)):
-    if answer_list[i] == max(answer_list):
-        answer_set.add(input_list[i])
-if len(answer_set) > 1:
+    if i not in input_dict:
+        input_dict[i] = 1
+    else:
+        input_dict[i] = input_dict[i] + 1
+#(배운 것) 딕셔너리 추가는 생각보다 간편하다. 그냥 키와 값을 선언해주면 된다.
+#(배운 것) 딕셔너리는 not in 등으로 있는지 체크할 때 특별히 언급해주지 않으면 key값 기준으로 체크한다.
+for i in input_dict:
+    if input_dict[i] == max(input_dict.values()):
+#(배운 것) 딕셔너리에서 값 기준으로 무언갈 하고 싶으면 .values()로 값을 사용할거라고 선언해준다.
+        answer_list.append(i)
+if len(answer_list) > 1:
     print("?")
 else:
-    print(list(answer_set)[0])
+    print(answer_list[0])
---
+++ 0529.py
@@ -0,0 +1,92 @@
+# 2022년 5월 29일 백준 알고리즘 풀이
+import sys
+
+# 1152번
+#a = 1
+##>.strip()
+#value = ' '.join(input_value.split()).strip()
+#for i in str(value):
+#    if i == " ":
+#        a = a + 1
+#print(a)
+
+# 위 처럼 푸는 게 아니라
+#>input_list = sys.stdin.readline().split()
+#>print(len(input_list))
+#(배운 것) 단어라면 단어의 성질에 초점을 맞춰서 푼다.
+
+
+
+
+
+
+
+# 2908번
+#>def reverse(a):
+#>    answer = ''
+#>    for i in str(a):
+#>        answer = i + answer
+#>        #(배운 것) 간단한 트릭으로 문자열을 뒤짚을 수 있다.
+#>    return answer
+
+#>A, B = map(int, sys.stdin.readline().split())
+#>if int(reverse(A)) < int(reverse(B)):
+#>    print(reverse(B))
+#>else:
+#>    print(reverse(A))
+
+
+
+
+
+
+# 5622번
+def to_num_sec(W):
+    if W == 'Z':
+        W = int(ord(W))-2
+    elif int(ord(W)) >= 83:
+        W = int(ord(W))-1
+    else:
+        W = int(ord(W))
+    W = (W - 59) // 3
+    return W + 1
+
+#>input_value = sys.stdin.readline().strip()
+#>answer = 0
+#>for i in str(input_value):
+#>    answer = answer + to_num_sec(i)
+#>print(answer)
+
+
+
+
+
+# 2941번
+#>croatia = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+#>input_value = sys.stdin.readline().strip()
+#>for i in croatia:
+#>    input_value = input_value.replace(i,'A')
+#>print(len(input_value))
+
+
+
+
+
+
+# 1316번
+def group_word(input_value):
+    for i in str(input_value):
+        while i*2 in input_value:
+            input_value = input_value.replace(i*2,i)
+    if len(input_value) == len(set(input_value)):
+        b = 1
+    else:
+        b = 0
+    return b
+
+N = int(sys.stdin.readline())
+input_list = [sys.stdin.readline().strip() for _ in range(N)]
+answer = 0
+for l in input_list:
+    answer = answer + group_word(l)
+print(answer)
